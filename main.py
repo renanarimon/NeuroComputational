b5df0b90e96ef1ad3ca38f8fd06e4a0592275b32
renanarimon <77155986+renanarimon@users.noreply.github.com>
2022-04-26 17:44:56 +0300

    score

--- main.py
+++ main.py
@@ -3,25 +3,22 @@ import numpy as np
 
 from Adaline import Adaline
 
+
 def createDataSet(num: int) -> (np.ndarray, np.ndarray):
     dataSet = np.ndarray((num, 3))
     for i in range(num):
         m = random.randint(-10000, 10000)
         n = random.randint(-10000, 10000)
-        dataSet[i, 0] = m/1000
-        dataSet[i, 1] = n/1000
-        dataSet[i, 2] = 1 if n/100 > 1 else -1
-    return dataSet[:, 0:2], dataSet[:,2]
+        dataSet[i, 0] = m / 1000
+        dataSet[i, 1] = n / 1000
+        dataSet[i, 2] = 1 if n / 100 > 1 else -1
+    return dataSet[:, 0:2], dataSet[:, 2]
 
 
 if __name__ == '__main__':
     X_train, y_train = createDataSet(1000)
-    print("x_train: \n", X_train)
-    print("y_train: \n", y_train)
-    # a= ada.AdalineAlgo(0.01, 100)
+    X_test, y_test = createDataSet(1000)
     a = Adaline(10, 0.01)
     a.fit(X_train, y_train)
-    pred = a.predict(X_train)
-    print("pred: \n", pred)
-    score = a.score(X_train, y_train)
+    score = a.score(X_test, y_test)
     print(score)
